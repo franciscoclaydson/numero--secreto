8c932ce7cf572e16ba2202d4d715f7df71e55f77
franciscoclaydson <francicoclaydson33@gmail.com>
2024-03-19 11:12:18 -0300

    projeto inicial

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogo do numero secreto';
+
+let paragráfo = document.querySelector('p');
+paragráfo.innerHTML = 'escolha um número entre 1 e 10';
+
+function  verificarChute() {
+    console.log('o botao foi clicado!');
+}
